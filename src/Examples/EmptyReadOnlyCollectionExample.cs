36781ebc0af06ff71be9c8a60fedc7cd344b6297
Peter Ritchie <peteraritchie@users.noreply.github.com>
2026-02-08 15:43:29 -0500

    chore: updated readme and release notes

--- src/Examples/EmptyReadOnlyCollectionExample.cs
+++ src/Examples/EmptyReadOnlyCollectionExample.cs
@@ -7,6 +7,8 @@ public static class EmptyReadOnlyCollectionExample
 {
 	public static IReadOnlyCollection<T> GetReadOnlyCollection<T>()
 	{
+#pragma warning disable IDE0301 // Simplify collection initialization
 		return IReadOnlyCollection<T>.Empty;
+#pragma warning restore IDE0301 // Simplify collection initialization
 	}
 }
